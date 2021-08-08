be0e1ab17df2bb59f1859802a3632605fed10fa3
Marco <marcoverbeek99@gmail.com>
2021-08-08 14:26:12 +0200

    Removed specs, added .env to gitignore (!!) and added Twisted npm package

--- src/analyses/analyses.controller.ts
+++ src/analyses/analyses.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AnalysesService } from './analyses.service';
+import { LolApi } from 'twisted';
+import { Regions } from 'twisted/dist/constants';
 
 @Controller('analyses')
 export class AnalysesController {
   constructor(private readonly analysesService: AnalysesService) {}
 
+  LeagueAPI = new LolApi();
+
   @Get(':gameId')
   async getAnalysis(@Param('gameId') gameId: string) {
+    const match = await this.LeagueAPI.Match.get(5399406298, Regions.EU_WEST);
+    console.log(match);
+
     return this.analysesService.analyseGameWithId(gameId);
   }
 }
