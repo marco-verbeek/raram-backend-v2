54e9179aee5c3efcfde4bb5b6ec9e25b03640461
Marco <marcoverbeek99@gmail.com>
2021-09-21 16:29:03 +0200

    Implemented logger middleware

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { logger } from './middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(logger);
 
   const config = new DocumentBuilder()
     .setTitle('rARAM API')
